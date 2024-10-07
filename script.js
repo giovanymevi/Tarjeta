47094569bc743d91e5a11a6cb7b88b947614c25a
giovannymen <giovannymendozavillamizar1@gmail.com>
2024-10-06 20:05:06 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('aboutMe').addEventListener('click', function() {
+    alert('Aquí puedes agregar más información sobre ti.');
+});
+
+document.getElementById('portfolio').addEventListener('click', function() {
+    alert('Aquí puedes agregar tu portafolio.');
+});
